6a4029a28a30662992ecdaa8eacf5bd9cab93e72
jonas <jonas.masharqa@gmail.com>
2019-10-17 15:01:03 +0200

    Refactors input fields and select input and creates InputFields component.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import DisplayCooperResult from './Components/DisplayCooperResult'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
       distance: '',
       gender: 'female',
       age: ''
     }
-  }
 
   onChange(event) {
+    debugger
     this.setState({
       [event.target.id]: event.target.value
     })
@@ -20,21 +18,7 @@ class App extends Component {
  render() {
     return (
       <>
-        <div>
-          <label>Distance</label>
-          <input id="distance" onChange={this.onChange.bind(this)}></input>
-        </div>
 
-        <select id="gender" onChange={this.onChange.bind(this)}>
-          <option value="female">Female</option>
-          <option value="male">Male</option>
-        </select>
-
-        <div>
-          <label>Age</label>
-          <input id="age" onChange={this.onChange.bind(this)}></input>
-        </div>
-        
           <DisplayCooperResult 
             distance={this.state.distance}
             gender={this.state.gender}
