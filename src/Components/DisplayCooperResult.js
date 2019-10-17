60cd151c0bb81f498b6698fa78f8b43ab203aa4c
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-10-17 18:58:34 +0200

    Adds saving Cooper results into Disply component and performance data modules.

--- src/Components/DisplayCooperResult.js
+++ src/Components/DisplayCooperResult.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CooperCalculator from '../Modules/CooperCalculator';
+import { saveData } from '../Modules/PerformanceData';
 
 class DisplayCooperResult extends Component {
 
@@ -7,15 +8,36 @@ class DisplayCooperResult extends Component {
     return CooperCalculator(this.props.distance, this.props.gender, this.props.age);
   }
 
+  async saveCooperData() {
+    const result = this.calculate();
+    try {
+      await saveData(result);
+      this.props.entryHandler();
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   render() {
     let results
-    if (this.props.age !== '' && this.props.distance !== '') {
-      results =
-      <div>
-        <p>{this.props.age} y/o {this.props.gender} running {this.props.distance} meters.</p>
-        <p>Result: {this.calculate()}</p>
-      </div>
+    let saveButton;
+
+    if (this.props.authenticated === true) {
+      saveButton = (
+        <>
+          <button id="save-result" onClick={this.saveCooperData.bind(this)}>Save entry</button>
+        </>
+      )
     }
+    if (this.props.age !== '' && this.props.distance !== '') {
+        results = (
+          <>
+            <p>{this.props.age} y/o {this.props.gender} running {this.props.distance} meters.</p> 
+            <p>Result: {this.calculate()}</p>
+            {saveButton}
+          </>
+        )
+      }
     return (
       <div>
         {results}
