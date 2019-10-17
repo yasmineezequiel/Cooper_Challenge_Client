4236c3fa8d8190855d547c67b98389b3ada54d48
jonas <jonas.masharqa@gmail.com>
2019-10-17 14:05:14 +0200

    Creates onChange function and passes in props to display component. Adds onChange in input fields.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,44 @@ import React, { Component } from 'react';
 import DisplayCooperResult from './Components/DisplayCooperResult'
 
 class App extends Component {
-  constructor(props) 
+  constructor(props) {
+    super(props);
+    this.state = {
+      distance: '',
+      gender: 'female',
+      age: ''
+    }
+  }
+
+  onChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
  render() {
     return (
       <>
         <div>
           <label>Distance</label>
-          <input id="distance"></input>
+          <input id="distance" onChange={this.onChange.bind(this)}></input>
         </div>
 
-        <select id="gender">
+        <select id="gender" onChange={this.onChange.bind(this)}>
           <option value="female">Female</option>
           <option value="male">Male</option>
         </select>
 
         <div>
           <label>Age</label>
-          <input id="age"></input>
+          <input id="age" onChange={this.onChange.bind(this)}></input>
         </div>
-        <div>
-          <DisplayCooperResult />
-        </div>
+        
+          <DisplayCooperResult 
+            distance={this.state.distance}
+            gender={this.state.gender}
+            age={this.state.age}
+            />
       </>
     );
   }
